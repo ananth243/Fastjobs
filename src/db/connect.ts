3a8c89f4263c8243b76e0b238596ef327dd89e58
Ananth Raghav <raghavananth02@gmail.com>
2023-04-27 18:48:01 +0600

    Added manual module of pg

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,15 +1,17 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 
 const DB_PATH = process.env.DB_PATH as string;
 
 export const sequelize = new Sequelize(DB_PATH, {
   dialect: "postgres",
+  dialectModule: pg,
 });
 
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    sequelize.sync({alter: true});
+    sequelize.sync({ alter: true });
   } catch (error) {
     process.exit(0);
   }
